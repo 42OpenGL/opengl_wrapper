40b8dea078f6fbbbb0c8058ce0aeea10d8a1325f
yeonseong <yeonseong.dev@gmail.com>
2022-10-19 15:04:13 +0900

    FEAT: buffer class

--- source/main.cpp
+++ source/main.cpp
@@ -26,6 +26,73 @@ struct Vertex
 	glm::vec3 color;
 };
 
+class Buffer
+	{
+		private:
+			std::vector<Vertex> vertices = {
+							{{ 0.5f,  0.5f,  0.0f}, {0.5f, 1.0f, 1.0f}},
+							{{ 0.5f, -0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
+							{{-0.5f, -0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
+							{{-0.5f,  0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
+						};
+			std::vector<GLuint> indices =  {
+							0, 1, 2,
+							1, 2, 3,
+						};
+			GLuint vertex_array_obj;
+			GLuint vertex_buffer_obj;
+			GLuint element_buffer_obj;
+		public:
+			Buffer()
+			{
+				glGenVertexArrays(1, &vertex_array_obj);
+				glGenBuffers(1, &vertex_buffer_obj);
+				glGenBuffers(1, &element_buffer_obj);
+				
+				glBindVertexArray(vertex_array_obj);
+				glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_obj);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_obj);
+				
+				glEnableVertexAttribArray(0);
+				glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+				glEnableVertexAttribArray(1);
+				glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+
+				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this->vertices.size(), this->vertices.data(), GL_STATIC_DRAW);
+				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * this->indices.size(), this->indices.data(), GL_STATIC_DRAW);
+			}
+			~Buffer()
+			{
+				glDeleteVertexArrays(1, &vertex_array_obj);
+				glDeleteBuffers(1, &vertex_buffer_obj);
+				glDeleteBuffers(1, &element_buffer_obj);
+			}
+
+			void setVertices(std::vector<Vertex> new_vertices)
+			{
+				this->vertices = new_vertices;
+				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this->vertices.size(), this->vertices.data(), GL_STATIC_DRAW);
+			}
+
+			void setIndices(std::vector<GLuint> new_indices)
+			{
+				this->indices = new_indices;
+				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * this->indices.size(), this->indices.data(), GL_STATIC_DRAW);
+			}
+			
+			GLuint getVAO()
+			{
+				return this->vertex_array_obj;
+			}
+
+			std::vector<GLuint> getIndices()
+			{
+				return this->indices;
+			}
+	};
+
+	
+
 int main_process()
 {
     GLWindowWrapper window_instance;
@@ -38,42 +105,23 @@ int main_process()
 	GLFWwindow* window = window_instance.getWindow();
 	GLuint shader_program = shaderprogram_instance.getShaderProgram();
 
+	Buffer			buffer_instance;
 
-	// 출력할 데이터
-	std::vector<Vertex> vertices = {
-		{{ 0.5f,  0.5f,  0.0f}, {0.5f, 1.0f, 1.0f}},
-		{{ 0.5f, -0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
-		{{-0.5f, -0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
-		{{-0.5f,  0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
-	};
-
-	std::vector<GLuint> indices = {
-		0, 1, 2,
+	std::vector<GLuint> new_indices = {
+		0, 2, 1,
 		1, 2, 3,
 	};
 
-	// 버퍼
-	GLuint vertex_array_obj;
-	GLuint vertex_buffer_obj;
-	GLuint element_buffer_obj;
+	std::vector<Vertex> new_vertices = {
+		{{ 0.5f,  0.5f,  0.0f}, {1.0f, 1.0f, 1.0f}},
+		{{ 0.5f, -0.5f,  0.0f}, {1.0f, 0.0f, 0.0f}},
+		{{-0.5f, -0.5f,  0.0f}, {0.0f, 1.0f, 0.0f}},
+		{{-0.5f,  0.5f,  0.0f}, {0.0f, 0.0f, 1.0f}},
+	};
 
-	glGenVertexArrays(1, &vertex_array_obj);
-	glGenBuffers(1, &vertex_buffer_obj);
-	glGenBuffers(1, &element_buffer_obj);
 	
-	glBindVertexArray(vertex_array_obj);
-	glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_obj);
-	
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
-	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_obj);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
-
+	buffer_instance.setIndices(new_indices);
+	buffer_instance.setVertices(new_vertices);
 
 	// 렌더링 루프
 	while (!glfwWindowShouldClose(window))
@@ -82,21 +130,17 @@ int main_process()
 		// 렌더링
 		// 버퍼 초기화
 		// 넘치면 1, 음수면 0으로 해줌
-		glClearColor(3232321.0f, -43141341.0f, -11241251325.2f, 1.0f);
+		glClearColor(0.2f, 0.2f, 0.2f, 0.2f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		// 그리기
 		glUseProgram(shader_program);
-		glBindVertexArray(vertex_array_obj);
-		glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, nullptr);
+		glBindVertexArray(buffer_instance.getVAO());
+		glDrawElements(GL_TRIANGLES, buffer_instance.getIndices().size(), GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	glDeleteVertexArrays(1, &vertex_array_obj);
-	glDeleteBuffers(1, &vertex_buffer_obj);
-	glDeleteBuffers(1, &element_buffer_obj);
 	glDeleteProgram(shader_program);
-
 	glfwTerminate();
 	return 0;
 }
