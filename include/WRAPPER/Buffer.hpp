6f9ae3f8f65060ca40ed512b5ab5e89dd9562b5d
maindishes <manynolan1@gmail.com>
2022-10-26 21:16:41 +0900

    STYLE:   add _ in member var

--- include/WRAPPER/Buffer.hpp
+++ include/WRAPPER/Buffer.hpp
@@ -12,48 +12,48 @@ class Buffer
 		// TODO: inline 으로 초기화
 		// TODO: buffer에도 여러개 받을 수 있게 확장하기
 		// TODO: vertices가 다양함. -> 여러 vertices를 대응할 수 있도록
-			std::vector<Vertex> vertices = {
+			std::vector<Vertex> _vertices = {
 							{{ 0.5f,  0.5f,  0.0f}, {0.5f, 1.0f, 1.0f}},
 							{{ 0.5f, -0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
 							{{-0.5f, -0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
 							{{-0.5f,  0.5f,  0.0f}, {0.0f, 0.0f, 0.0f}},
 						};
-			std::vector<GLuint> indices =  {
+			std::vector<GLuint> _indices =  {
 							0, 1, 2,
 							1, 2, 3,
 						};
-			GLuint vertex_array_obj;
-			GLuint vertex_buffer_obj;
-			GLuint element_buffer_obj;
+			GLuint _vertex_array_obj;
+			GLuint _vertex_buffer_obj;
+			GLuint _element_buffer_obj;
 		public:
-			Buffer(): vertex_array_obj(0), vertex_buffer_obj(0), element_buffer_obj(0)
+			Buffer() : _vertex_array_obj(0), _vertex_buffer_obj(0), _element_buffer_obj(0)
 			{
 				// TODO: 생성자 초기화
-				glGenVertexArrays(1, &vertex_array_obj);
-				glGenBuffers(1, &vertex_buffer_obj);
-				glGenBuffers(1, &element_buffer_obj);
+				glGenVertexArrays(1, &_vertex_array_obj);
+				glGenBuffers(1, &_vertex_buffer_obj);
+				glGenBuffers(1, &_element_buffer_obj);
 				
-				glBindVertexArray(vertex_array_obj);
-				glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_obj);
-				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, element_buffer_obj);
+				glBindVertexArray(_vertex_array_obj);
+				glBindBuffer(GL_ARRAY_BUFFER, _vertex_buffer_obj);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _element_buffer_obj);
 				
 				glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
 				glEnableVertexAttribArray(0);
 				glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
 				glEnableVertexAttribArray(1);
 
-				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this->vertices.size(), this->vertices.data(), GL_STATIC_DRAW);
-				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * this->indices.size(), this->indices.data(), GL_STATIC_DRAW);
+				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this->_vertices.size(), this->_vertices.data(), GL_STATIC_DRAW);
+				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * this->_indices.size(), this->_indices.data(), GL_STATIC_DRAW);
 			}
 			~Buffer()
 			{
 				// TODO: 동적할당, 동적해제할 떄 가드하기!!
-				if (!vertex_array_obj)
-					glDeleteVertexArrays(1, &vertex_array_obj);
-				if (!vertex_buffer_obj)
-					glDeleteBuffers(1, &vertex_buffer_obj);
-				if (!element_buffer_obj)
-					glDeleteBuffers(1, &element_buffer_obj);
+				if (!_vertex_array_obj)
+					glDeleteVertexArrays(1, &_vertex_array_obj);
+				if (!_vertex_buffer_obj)
+					glDeleteBuffers(1, &_vertex_buffer_obj);
+				if (!_element_buffer_obj)
+					glDeleteBuffers(1, &_element_buffer_obj);
 			}
 
 			// TODO: 레퍼런스로 받기
@@ -61,24 +61,24 @@ class Buffer
 			{
 				std::cout << (long)&new_vertices[0] << std::endl;
 				// swap -> 보류. 
-				this->vertices = new_vertices;
-				std::cout << (long)&(this->vertices[0]) << std::endl;
-				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this->vertices.size(), this->vertices.data(), GL_STATIC_DRAW);
+				this->_vertices = new_vertices;
+				std::cout << (long)&(this->_vertices[0]) << std::endl;
+				glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * this->_vertices.size(), this->_vertices.data(), GL_STATIC_DRAW);
 			}
 
 			void setIndices(const std::vector<GLuint> &new_indices)
 			{
-				this->indices = new_indices;
-				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * this->indices.size(), this->indices.data(), GL_STATIC_DRAW);
+				this->_indices = new_indices;
+				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * this->_indices.size(), this->_indices.data(), GL_STATIC_DRAW);
 			}
 			
 			GLuint getVAO()
 			{
-				return this->vertex_array_obj;
+				return this->_vertex_array_obj;
 			}
 
 			std::vector<GLuint> getIndices()
 			{
-				return this->indices;
+				return this->_indices;
 			}
 	};
